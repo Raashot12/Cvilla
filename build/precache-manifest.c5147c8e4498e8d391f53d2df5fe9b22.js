6944cf0027d07e46d845002af847b7be0400ebc0
Iskilu Rasheed Akanni <akannirasheed1996@gmail.com>
2020-08-17 19:31:01 +0100

    Updated buildup

---
+++ build/precache-manifest.c5147c8e4498e8d391f53d2df5fe9b22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12f088f5cf0fd0476b96addc7fa6048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edccb544d26f01cd04f8",
+    "url": "/static/css/main.b36e51b6.chunk.css"
+  },
+  {
+    "revision": "552eb0a543e2cac1b8d9",
+    "url": "/static/js/2.1a45e7f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a45e7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edccb544d26f01cd04f8",
+    "url": "/static/js/main.60461e77.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "4020ffe95c10b083bf0fcceb165da551",
+    "url": "/static/media/home.4020ffe9.jpg"
+  },
+  {
+    "revision": "9843d90de04959c169c72247aac9b188",
+    "url": "/static/media/logo.9843d90d.png"
+  },
+  {
+    "revision": "be37cdd899fc4c7b22c93e4632f192fc",
+    "url": "/static/media/man.be37cdd8.png"
+  }
+]);
