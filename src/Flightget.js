c57d0c5b3432799b8837eab0343a5d398de28fa8
Iskilu Rasheed Akanni <akannirasheed1996@gmail.com>
2020-08-17 19:46:04 +0100

    updated the Bounce on my api

--- src/Flightget.js
+++ src/Flightget.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import Bounce from 'react-reveal'
 
 const Getit = ({ routes }) => {
 	const air = routes.map((route, i) => {
-		return <div key={i} className="rocket1">
+       		return<div key={i} className="rocket1">
+			<Bounce left duration={3000}>
 			<h1> Flight Name:{route.destination.name}</h1>
 			<h2> Flight Municipality:{route.destination.municipalityName}</h2>
 			<h2> Daily Flight by Average:{route.averageDailyFlights}</h2>
 			<h2 className='rocket' key={i}> Flight Operator Name:{route.operators.map((operator, i) => {
 				return <p key={i}> Operator Name:{operator.name}</p>
 			})}</h2>
+						</Bounce>
 		</div>
+		
 	})
 
 	return (
